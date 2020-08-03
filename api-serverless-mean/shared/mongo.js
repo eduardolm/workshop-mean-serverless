82bcdcc35c774eab08075711c6c6682f113e6c59
Eduardo <lodi001@uol.com.br>
2020-08-02 21:17:39 -0300

    Created simple CRUD using Azure Functions

---
+++ api-serverless-mean/shared/mongo.js
@@ -0,0 +1,23 @@
+/**
+ * Arquivo: mongo.js
+ * Data: 02/08/2020
+ * Descrição: arquivo responsável por tratar a conexão da Base de Dados localmente
+ */
+
+ const {MongoClient} = require('mongodb')
+
+ const config = {
+     url: 'mongodb://localhost:27017/crud-workshop-mean-az',
+     dbName: 'crud-workshop-mean-az'
+ }
+
+ async function createConnection () {
+     const connection = await MongoClient.connect(config.url, {useNewUrlParser: true})
+     const db = connection.db(config.dbName)
+     return {
+         connection,
+         db
+     }
+ }
+
+ module.exports = createConnection
--- front/src/app/funcionario.service.ts
+++ front/src/app/funcionario.service.ts
@@ -6,8 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FuncionarioService {
 
-  // ==> Uri da api (Back-End)
-  uri = 'http://localhost:8000/api';
+  // ==> Uri da api (Back-End) - Era 8000
+  uri = 'http://localhost:7071/api';
 
   constructor(private http: HttpClient) { }
 
@@ -22,7 +22,7 @@ export class FuncionarioService {
     };
     console.log(funcionario);
 
-    // ==> (POST - Url no Back-End): http://localhost:8000/api/funcionario
+    // ==> (POST - Url no Back-End): http://localhost:8000/api/funcionarios
     this
       .http
       .post(`${this.uri}/funcionarios`, funcionario)
